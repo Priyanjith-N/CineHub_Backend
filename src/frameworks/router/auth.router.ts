2874ade89cca7c26610c90b183a4a4b0cf06b353
Priyanjith N <jithpriyan2006@gmail.com>
2024-05-31 18:07:38 +0530

    basic folder and files setup

--- src/frameworks/router/auth.router.ts
+++ src/frameworks/router/auth.router.ts
@@ -1,14 +1,20 @@
 import express, { NextFunction, Request, Response, Router } from 'express';
 import AuthController from '../../adapters/controllers/auth.controller';
 import AuthUseCase from '../../usecase/auth.usecase';
+import AuthRepository from '../../adapters/repositories/auth.repositories';
+
+// importing collections
+import Users from '../models/user.model';
 
 // interfaces
 import { IAuthController } from '../../interface/controllers/IAuth.controller';
 import IAuthUseCase from '../../interface/usecase/IAuth.usecase';
+import IAuthRepository from '../../interface/repositories/IAuth.repositories';
 
 const router: Router = express.Router();
 
-const authUseCase: IAuthUseCase = new AuthUseCase();
+const authRepository: IAuthRepository = new AuthRepository(Users);
+const authUseCase: IAuthUseCase = new AuthUseCase(authRepository);
 const authController: IAuthController = new AuthController(authUseCase);
 
 
--- src/usecase/auth.usecase.ts
+++ src/usecase/auth.usecase.ts
@@ -1,9 +1,18 @@
+import { IUserDocument } from "../interface/collections/IUsers.collections";
+import IAuthRepository from "../interface/repositories/IAuth.repositories";
 import IAuthUseCase from "../interface/usecase/IAuth.usecase";
 
 export default class AuthUseCase implements IAuthUseCase {
-    constructor() {}
+    private authRepository: IAuthRepository;
+    constructor(authRepository: IAuthRepository) {
+        this.authRepository = authRepository;
+    }
 
     async authenticateUser(email: string, password: string): Promise<void> {
-        console.log('here');
+        const userData: IUserDocument | null = await this.authRepository.getDataByEmail(email);
+
+        if(!userData){
+            // no user with that email already exist
+        }
     }
 }
