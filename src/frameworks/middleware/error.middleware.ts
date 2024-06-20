5f4b599e08fd7b9896f30c19d2d42c5063be2c48
Priyanjith N <jithpriyan2006@gmail.com>
2024-06-20 16:46:56 +0530

    error fixed the response

--- src/frameworks/middleware/error.middleware.ts
+++ src/frameworks/middleware/error.middleware.ts
@@ -1,10 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import AuthenticationError, { ErrorDetails } from "../../errors/authentication.error";
 import { StatusCodes } from "../../enums/statusCode.enum";
+import jwt from 'jsonwebtoken';
 
 export default function errorHandler(err: ErrorDetails, req: Request, res: Response, next: NextFunction) {
     if(err instanceof AuthenticationError) {
         res.status(err.details.statusCode!).json({message: err.message, errorField: err.details.errorField});
+    }else if(err instanceof jwt.TokenExpiredError) {
+        res.status(401).json({ message: 'Token expired' });
     }else{
         console.error(err); // Log entire error object
         res.status(StatusCodes.InternalServer).json({ error: 'Internal Server Error.' });
